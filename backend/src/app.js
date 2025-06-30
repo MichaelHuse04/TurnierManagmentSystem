11ca8f62226b60efeeaa1f4dd7a57fe144b97051
Michael Huse <michael.huse@wolters-rundreisen.de>
2025-06-30 11:43:22 +0200

    stuff

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,12 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const express = require("express");
 const server = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5713;
 
+
+server.get("/",(req, res) => {
+    res.send("hello world!");
+});
 server.listen(port, () => {
     console.log("Listening on port: " + port);
 });
