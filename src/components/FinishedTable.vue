435c5be72a3b0d3958ea9b3d28a3a463f264036b
Michael Huse <michael.huse@wolters-rundreisen.de>
2025-07-05 21:36:04 +0200

    renamed FinishedView.vue ro FinishedTable.vue

---
+++ src/components/FinishedTable.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+/*
+import type {Player} from "@/stores/typs.ts";
+import {computed} from "vue";
+
+const props = defineProps<{
+  matchUps?: Player[][],
+}>();
+
+
+const players = props.matchUps.flat(2);
+
+const ratings: Map<string, Player[]> = new Map();
+
+
+ratings.set("1", players.filter((player) => {return player.hasWonGame === true}));*/
+
+</script>
+
+<template>
+<h1>Done!</h1>
+</template>
+
+<style scoped>
+
+</style>
